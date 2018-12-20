53c8d01cf0a5a31739428ff342a8c5f1d81fdb1c
Awin Abi <awin@awinawin.awin>
2018-12-20 23:26:11 +0530

    Add multiple functions to lib

--- intro/math.rb
+++
@@ -1,5 +0,0 @@
-def exp(n)
-  puts "Calling exp() with args: #{n}"
-  Math.exp(n)
-end
-
---
+++ intro/mylib.rb
@@ -0,0 +1,12 @@
+def ruby_exp(n)
+  puts "Calling ruby_exp() with args: #{n}"
+  Math.exp(n)
+end
+
+def ruby_log(n)
+  puts "Calling ruby_log() with args: #{n}"
+  Math.log(n)
+end
+
+# puts e=ruby_exp(3)
+# puts ruby_log(e)
